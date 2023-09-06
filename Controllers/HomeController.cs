d77298b07c01880ab6e3cceb060102adb11574ed
Home <home@Homes-iMac.local>
2023-09-06 21:05:55 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using AdminPanelTutorial.Models;
+
+namespace AdminPanelTutorial
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
